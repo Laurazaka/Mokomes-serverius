fc2f1be101eaf7fa8440f716eab28cf76cad01fc
Laurazaka <98113176+Laurazaka@users.noreply.github.com>
2022-03-23 20:38:54 +0200

    Page routes

--- pages/PageHome.js
+++ pages/PageHome.js
@@ -1,2 +1,10 @@
 import { PageTemplate } from "../lib/PageTemplate.js";
 
+class PageHome extends PageTemplate {
+    mainHTML() {
+        return `HOME PAGE CONTENT`;
+    }
+
+}
+
+export { PageHome}
---
+++ pages/PageRegister.js
@@ -0,0 +1,10 @@
+import { PageTemplate } from "../lib/PageTemplate.js";
+
+class PageRegister extends PageTemplate {
+    mainHTML() {
+        return `REGISTER HOME PAGE CONTENT`;
+    }
+
+}
+
+export { PageRegister }
